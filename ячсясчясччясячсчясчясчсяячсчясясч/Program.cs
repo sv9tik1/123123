da9e30f2a396d70861c76cfcf87ab1d849f5a490
saidPREDATOR <112885798+sv9tik1@users.noreply.github.com>
2023-12-07 21:41:23 +0300

    Add files via upload

---
+++ ячсясчясччясячсчясчясчсяячсчясясч/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        string image = "image/typoikot.jpg";
+        string myString = image.ToString();
+        byte[] imageByt = File.ReadAllBytes(image);
+        string outputImage = "image/typoikot.jpg";
+        File.WriteAllBytes(outputImage, imageByt);
+        Console.WriteLine("Изображение успешно сохранено по пути: " + outputImage);
+    }
+} 
